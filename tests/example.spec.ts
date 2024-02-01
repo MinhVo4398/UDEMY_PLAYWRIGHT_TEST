b8516b8580f4e0e9c215d29fe8e5be64b6216a2c
Minh Vo <minh.vo@noveogroup.com>
2024-02-01 15:36:19 +0700

    hook

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,13 +1,13 @@
-import {test, expect} from '@playwright/test'
+import { test, expect } from '@playwright/test'
 
-test("Simple basic test", async({page})=>{
+test("Simple basic test", async ({ page }) => {
     //Here goes the test code
-    await page.goto("https://example.com/") 
+    await page.goto("https://example.com/")
     const pageTitle = await page.locator("h1")
     await expect(pageTitle).toContainText("Example Domain")
 })
 
-test("Clicking on Elements",async ({page}) => {
+test("Clicking on Elements", async ({ page }) => {
     await page.goto("http://zero.webappsecurity.com/")
     await page.click("#signin_button")
     await page.click("text=Sign in")
@@ -17,7 +17,7 @@ test("Clicking on Elements",async ({page}) => {
 
 })
 
-test.skip("Selectors",async ({page}) => {
+test.skip("Selectors", async ({ page }) => {
     // text
     await page.click("text=some text")
 
@@ -36,43 +36,51 @@ test.skip("Selectors",async ({page}) => {
     await page.click("//button")
 })
 
-test.describe("My first test suite", ()=> {
-    test("Working with Inputs @myTag",async ({page}) => {
+test.describe("My first test suite", () => {
+    test("Working with Inputs @myTag", async ({ page }) => {
         await page.goto("http://zero.webappsecurity.com/")
         await page.click("#signin_button")
-    
-        await page.fill("#user_login","some username")
+
+        await page.fill("#user_login", "some username")
         await page.fill("#user_password", "some password")
         await page.click("text=Sign in")
-    
+
         const errorMessage = await page.locator(".alert-error")
         await expect(errorMessage).toContainText("Login and/or password are wrong.")
     })
-    
-    test("Assertions @myTag",async ({page}) => {
+
+    test("Assertions @myTag", async ({ page }) => {
         await page.goto("https://www.example.com/");
-        await expect(page).toHaveURL("https://www.example.com/");   
+        await expect(page).toHaveURL("https://www.example.com/");
         await expect(page).toHaveTitle("Example Domain");
-    
+
         const element = await page.locator("h1");
         await expect(element).toBeVisible();
         await expect(element).toHaveText("Example Domain");
         await expect(element).toHaveCount(1);
-    
+
         const nonExistingElement = await page.locator("h5");
         await expect(nonExistingElement).not.toBeVisible();
-    }) 
-})
-  
-test.only("Screenshots",async ({page}) => {
-    // 1. step is load website
-    await page.goto("https://www.example.com/");
-    //2. take screenshot of full page
-    await page.screenshot({path: "screenshot.png", fullPage: true})
+    })
 })
 
-test.only("Single element screenshot", async ({page}) => {
-    await page.goto("https://www.example.com/");
-    const element = await page.$("h1")
-    await element?.screenshot({path:"single_element_screenshot.png"}) 
+test.describe.only("Hooks", () => {
+    test.beforeEach(async ({ page }) => {
+        // 1. step is load website
+        await page.goto("https://www.example.com/");
+    })
+
+    test.afterEach(async ({page}) => {
+        
+    })
+    test("Screenshots", async ({ page }) => {
+
+        //2. take screenshot of full page
+        await page.screenshot({ path: "screenshot.png", fullPage: true })
+    })
+
+    test("Single element screenshot", async ({ page }) => {
+        const element = await page.$("h1")
+        await element?.screenshot({ path: "single_element_screenshot.png" })
+    })
 })
