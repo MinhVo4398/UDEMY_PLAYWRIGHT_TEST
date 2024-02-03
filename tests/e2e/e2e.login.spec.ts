246a9ab39f6eb6407f35e38e558a3ffa51a878c7
Minh Vo <minh.vo@noveogroup.com>
2024-02-03 09:38:48 +0700

    refractor home page with pom

--- tests/e2e/e2e.login.spec.ts
+++ tests/e2e/e2e.login.spec.ts
@@ -1,34 +1,33 @@
-import { test, expect } from '@playwright/test'
+import { test, expect } from "@playwright/test";
+import { LoginPage } from "../../page-objects/LoginPage";
 
-test.describe("Login / Logout Flow ", () => {
-    //Before Hook
+test.describe.parallel.only("Login / Logout Flow ", () => {
+  let loginPage: LoginPage;
 
-    test.beforeEach(async ({ page }) => {
-        await page.goto("http://zero.webappsecurity.com/")
-    })
+  //Before Hook
+  test.beforeEach(async ({ page }) => {
+    loginPage = new LoginPage(page);
+    await loginPage.visit();
+  });
 
-    // Negative scenario
-    test("Negative Scenario for Login", async ({ page }) => {
-        await page.click("#signin_button");
-        await page.fill("#user_login", "invalid user name");
-        await page.fill("#user_password", "invalid password");
-        await page.click("text=Sign in");
+  // Negative scenario
+  test("Negative Scenario for Login", async ({ page }) => {
+    await page.click("#signin_button");
+    await loginPage.login("invalid username", "invalid password");
+    await loginPage.assertErrorMessage();
+  });
+  // Positive scenario + Logout
 
-        const errorMessage = await page.locator(".alert-error")
-        await expect(errorMessage).toContainText("Login and/or password are wrong.");
-    })
-    // Positive scenario + Logout
+  test("Postive Scenario for Login + Logout", async ({ page }) => {
+    await page.click("#signin_button");
+    await loginPage.login("username", "password");
 
-    test("Postive Scenario for Login + Logout", async ({ page }) => {
-        await page.click("#signin_button");
-        await page.fill("#user_login", "username");
-        await page.fill("#user_password", "password");
-        await page.click("text=Sign in");
-
-        const accountSummaryTab = await page.locator('#account_summary_tab');
-        if (!await accountSummaryTab.isVisible()) {
-            await page.goto('http://zero.webappsecurity.com/bank/account-activity.html');
-        };
-        await expect(accountSummaryTab).toBeVisible();
-    })
-})
+    const accountSummaryTab = await page.locator("#account_summary_tab");
+    if (!(await accountSummaryTab.isVisible())) {
+      await page.goto(
+        "http://zero.webappsecurity.com/bank/account-activity.html"
+      );
+    }
+    await expect(accountSummaryTab).toBeVisible();
+  });
+});
