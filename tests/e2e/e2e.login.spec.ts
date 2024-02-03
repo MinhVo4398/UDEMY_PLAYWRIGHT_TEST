28f02f3270f1e47d396d510e73ff38800be87c62
Minh Vo <minh.vo@noveogroup.com>
2024-02-03 10:08:12 +0700

    create homepage

--- tests/e2e/e2e.login.spec.ts
+++ tests/e2e/e2e.login.spec.ts
@@ -1,25 +1,29 @@
 import { test, expect } from "@playwright/test";
 import { LoginPage } from "../../page-objects/LoginPage";
+import { HomePage } from "../../page-objects/HomePage";
 
 test.describe.parallel.only("Login / Logout Flow ", () => {
   let loginPage: LoginPage;
+  let homePage: HomePage;
 
   //Before Hook
   test.beforeEach(async ({ page }) => {
     loginPage = new LoginPage(page);
-    await loginPage.visit();
+    homePage = new HomePage(page);
+
+    await homePage.visit();
   });
 
   // Negative scenario
   test("Negative Scenario for Login", async ({ page }) => {
-    await page.click("#signin_button");
+    await homePage.clickOnSignIn();
     await loginPage.login("invalid username", "invalid password");
     await loginPage.assertErrorMessage();
   });
   // Positive scenario + Logout
 
   test("Postive Scenario for Login + Logout", async ({ page }) => {
-    await page.click("#signin_button");
+    await homePage.clickOnSignIn();
     await loginPage.login("username", "password");
 
     const accountSummaryTab = await page.locator("#account_summary_tab");
