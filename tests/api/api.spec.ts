db7b648b78c34c48bd6bbf0b3c7ceedb1e6f8033
Minh Vo <minh.vo@noveogroup.com>
2024-02-04 15:50:53 +0700

    get request assert

--- tests/api/api.spec.ts
+++ tests/api/api.spec.ts
@@ -3,12 +3,29 @@ import {test,expect} from '@playwright/test';
 test.describe.parallel("API Testing", ()=>{
     const baseUrl = "https://reqres.in/api";
     test("Simple API Test - Assert Response Status", async ({request}) =>{
-        const response = await request.get(`${baseUrl}/users/2`);
+        const response = await request.get(`${baseUrl}/users/3`);
         expect(response.status()).toBe(200);
+
+        const resposeBody = JSON.parse(await response.text());
+        console.log(resposeBody);
+
     })
 
     test("Simple API Test- Assert Invalid Endpoint", async({request})=>{
         const response = await request.get(`${baseUrl}/users/non-existing-endpoint`);
         expect(response.status()).toBe(404);
     })
+
+    test.only("GET Request - Get User Detail", async ({request}) => {
+        const response = await request.get(`${baseUrl}/users/1`);
+        const responseBody = JSON.parse(await response.text());
+
+        expect(response.status()).toBe(200);
+        expect(responseBody.data.id).toBe(1);
+        expect(responseBody.data.first_name).toBe("George");
+        expect(responseBody.data.last_name).toBe("Bluth");
+        expect(responseBody.data.email).toBeTruthy();
+        console.log(responseBody);
+    })
 })
+
